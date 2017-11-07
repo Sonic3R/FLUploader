8fda7b06f63220813e8ad00cc50d5c5a87abd406
mteodorescu <mteodorescu@EN614592>
2017-11-07 10:46:11 +0200

    ftp

---
+++ FLUploaderWebCore/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceCore;
+
+namespace FLUploaderWebCore.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            FTPManager man = new FTPManager();
+            man.Connect();
+
+            return Content("test");
+        }
+    }
+}
